c5bb497a8e1bc650b98b0c5012ca52bdb3b92137
Steven Oviedo <78757400+saoviedo3@users.noreply.github.com>
2024-02-22 17:43:58 -0500

    Agregar Funcionalidad Filtros

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [filter, setFilter] = useState('none'); // Cambiar el estado inicial a 'none'
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,7 +12,20 @@ const RepoList = ({ username }) => {
         const response = await axios.get(
           `https://api.github.com/users/${username}/repos`
         );
-        const sortedRepos = response.data.sort((a, b) => b.size - a.size);
+        let sortedRepos;
+        switch(filter) { // Filtrar repositorios basado en el estado del filtro
+          case 'stars':
+            sortedRepos = response.data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+            break;
+          case 'created':
+            sortedRepos = response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+            break;
+          case 'language':
+            sortedRepos = response.data.sort((a, b) => b.language - a.language);
+            break;
+          default:
+            sortedRepos = response.data; // Si el filtro es 'none' o cualquier otro valor, no se aplica ningún filtro
+        }
         const topRepos = sortedRepos.slice(0, 5);
         setRepos(topRepos);
       } catch (error) {
@@ -20,15 +34,25 @@ const RepoList = ({ username }) => {
     };
 
     fetchData();
-  }, [username]);
+  }, [username, filter]); // Agregar el filtro como dependencia
+
+  const handleFilterChange = (event) => { // Nueva función para manejar los cambios en el filtro
+    setFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Top 5 repositorios con más participación de {username}</h2>
+      <select onChange={handleFilterChange}> // Selector para cambiar el filtro
+        <option value="none">Ningún filtro</option> // Agregar la opción de 'Ningún filtro'
+        <option value="stars">Estrellas</option>
+        <option value="created">Fecha de creación</option>
+        <option value="language">Idioma predominante</option>
+      </select>
       <ul>
         {repos.map((repo) => (
           <li key={repo.id}>
-            {repo.name} - Tamaño: {repo.size}
+            {repo.name} - Tamaño: {repo.size} - Estrellas: {repo.stargazers_count} - Lenguaje: {repo.language} - Fecha de creación: {repo.created_at}
           </li>
         ))}
       </ul>
