37e96a74e04900a8ffc1e57901eab6e4a24026c0
Steven Oviedo <78757400+saoviedo3@users.noreply.github.com>
2024-02-22 18:17:06 -0500

    funcionalidad Paginacion

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import { Slide } from "@mui/material";
-import { Select, MenuItem } from "@mui/material";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Slide, Select, MenuItem, Card, CardContent, Typography, Pagination } from "@mui/material";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
-  const [filter, setFilter] = useState('none'); // Cambiar el estado inicial a 'none'
+  const [filter, setFilter] = useState('none');
+  const [currentPage, setCurrentPage] = useState(1); // Nuevo estado para la página actual
+  const reposPerPage = 5; // Cantidad de repositorios por página
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,7 +16,7 @@ const RepoList = ({ username }) => {
           `https://api.github.com/users/${username}/repos`
         );
         let sortedRepos;
-        switch(filter) { // Filtrar repositorios basado en el estado del filtro
+        switch(filter) {
           case 'stars':
             sortedRepos = response.data.sort((a, b) => b.stargazers_count - a.stargazers_count);
             break;
@@ -24,28 +24,37 @@ const RepoList = ({ username }) => {
             sortedRepos = response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
             break;
           case 'language':
-            sortedRepos = response.data.sort((a, b) => b.language - a.language);
+            sortedRepos = response.data.sort((a, b) => a.language.localeCompare(b.language));
             break;
           default:
-            sortedRepos = response.data; // Si el filtro es 'none' o cualquier otro valor, no se aplica ningún filtro
+            sortedRepos = response.data;
         }
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        setRepos(sortedRepos);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
     };
 
     fetchData();
-  }, [username, filter]); // Agregar el filtro como dependencia
+  }, [username, filter]);
 
-  const handleFilterChange = (event) => { // Nueva función para manejar los cambios en el filtro
+  const handleFilterChange = (event) => {
     setFilter(event.target.value);
+  };
+
+  // Calcular los repositorios que se deben mostrar en la página actual
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
+  const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
+
+  // Cambiar la página
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
   };
 
   return (
     <div>
-      <h2>Top 5 repositorios con más participación de {username}</h2>
+      <h2>Repositorios de {username}</h2>
       <Select value={filter} onChange={handleFilterChange}>
         <MenuItem value="none">Ningún filtro</MenuItem>
         <MenuItem value="stars">Estrellas</MenuItem>
@@ -53,20 +62,21 @@ const RepoList = ({ username }) => {
         <MenuItem value="language">Idioma predominante</MenuItem>
       </Select>
       <ul>
-        {repos.map((repo) => (
+        {currentRepos.map((repo) => (
           <Slide direction="up" in={true} mountOnEnter unmountOnExit>
-          <Card key={repo.id} sx={{ marginBottom: "10px" }}>
-            <CardContent>
-              <Typography variant="h6">{repo.name}</Typography>
-              <Typography color="text.secondary">Tamaño: {repo.size}</Typography>
-              <Typography color="text.secondary">Estrellas: {repo.stargazers_count}</Typography>
-              <Typography color="text.secondary">Lenguaje: {repo.language}</Typography>
-              <Typography color="text.secondary">Fecha de creación: {new Date(repo.created_at).toLocaleDateString()}</Typography>
-            </CardContent>
-          </Card>
+            <Card key={repo.id} sx={{ marginBottom: "10px" }}>
+              <CardContent>
+                <Typography variant="h6">{repo.name}</Typography>
+                <Typography color="text.secondary">Tamaño: {repo.size}</Typography>
+                <Typography color="text.secondary">Estrellas: {repo.stargazers_count}</Typography>
+                <Typography color="text.secondary">Lenguaje: {repo.language}</Typography>
+                <Typography color="text.secondary">Fecha de creación: {new Date(repo.created_at).toLocaleDateString()}</Typography>
+              </CardContent>
+            </Card>
           </Slide>
         ))}
       </ul>
+      <Pagination count={Math.ceil(repos.length / reposPerPage)} page={currentPage} onChange={handlePageChange} />
     </div>
   );
 };
